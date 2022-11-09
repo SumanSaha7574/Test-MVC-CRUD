990e0a14b67a4fd34d1d564a2777015b937c9124
Suman Saha <sumansaha7475@gmail.com>
2022-11-10 03:45:59 +0530

    Add project files.

---
+++ BAL/BusinessLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Test_MVC_CRUD.DataAccess;
+
+namespace Test_MVC_CRUD.BAL
+{
+    public class BusinessLayer
+    {
+    }
+}
